99b90cc889cc8650ca3eecfac5b41a9d354155d8
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-10 15:05:08 -0400

    added musician poco

---
+++ Music_InstrumentDB_Console/POCO/Musician.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_InstrumentDB_Console.POCO
+{
+    public class Musician
+    {
+        public int FamousMusicianId { get; set; }
+        public string FullName { get; set; }
+        public int InstrumentId { get; set; }
+        public string MusicGenre { get; set; }
+        public string Description { get; set; }
+    }
+}
