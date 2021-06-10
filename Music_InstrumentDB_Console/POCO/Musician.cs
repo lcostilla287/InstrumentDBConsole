2137fb7e128641c9973f2e50811060dcf0ab1914
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-10 15:34:28 -0400

    added json props

--- Music_InstrumentDB_Console/POCO/Musician.cs
+++ Music_InstrumentDB_Console/POCO/Musician.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,19 @@ namespace Music_InstrumentDB_Console.POCO
 {
     public class Musician
     {
+        [JsonProperty("FamousMusicianId")]
         public int FamousMusicianId { get; set; }
+
+        [JsonProperty("FullName")]
         public string FullName { get; set; }
+
+        [JsonProperty("InstrumentId")]
         public int InstrumentId { get; set; }
+
+        [JsonProperty("MusicGenre")]
         public string MusicGenre { get; set; }
+
+        [JsonProperty("Description")]
         public string Description { get; set; }
     }
 }
