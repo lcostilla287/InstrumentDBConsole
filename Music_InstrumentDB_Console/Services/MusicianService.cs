f51be9ba8693ba086d5415fba610123055f8221e
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-11 14:29:37 -0400

    musician services using statements

--- Music_InstrumentDB_Console/Services/MusicianService.cs
+++ Music_InstrumentDB_Console/Services/MusicianService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Music_InstrumentDB_Console.POCO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
